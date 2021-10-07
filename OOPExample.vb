77be3be324a9caa5beb7aaabefdfdb427163846e
rosstimo <rosstimo@isu.edu>
2021-10-07 06:23:54 -0600

    add some inheritance examples

--- OOPExample.vb
+++ OOPExample.vb
@@ -24,6 +24,10 @@ Module OOPExample
             Return ""
         End Function
 
+        Public Function Speak() As String
+            Return ""
+        End Function
+
     End Class
 
     Class Mammal
@@ -32,6 +36,10 @@ Module OOPExample
         'Properties
         Public Overrides Property skinType As String = "Fur"
 
+        'Methods
+        Public Shadows Function Move() As String
+            Return "Walking on all fours"
+        End Function
     End Class
 
     Class Bird
@@ -39,6 +47,7 @@ Module OOPExample
         'Properties
         Public Overrides Property skinType As String = "Feathers"
 
+        'Methods
         Public Shadows Function Move() As String
             Return "Soaring through the sky..."
         End Function
@@ -54,32 +63,61 @@ Module OOPExample
 
         'Methods
         Sub New()
-
+            Me.skinType = "shiny coat"
         End Sub
 
         Public Shadows Function Move() As String
             Return "Leaping through the tall grass in the back yard.."
         End Function
 
+        Public Shadows Function Speak() As String
+            Return "Woof! Woof"
+        End Function
+
     End Class
 
     Class Cat
         Inherits Mammal
+        Public Shadows Function Move() As String
+            Return "Walking across the kitchen table..."
+        End Function
+
+        Public Shadows Function speak() As String
+            Return "Meow..."
+        End Function
     End Class
 
     Class Parrot
         Inherits Bird
+
+        Sub New()
+            Me.skinType = "Green Feathers"
+        End Sub
+
         Public Shadows Function Move() As String
             Return "Ok, who left the window open?"
+        End Function
+
+        Public Shadows Function speak() As String
+            Return "Pretty Bird...."
         End Function
     End Class
 
     Class Lion
         Inherits Mammal
+        Public Shadows Function speak() As String
+            Return "Roar!"
+        End Function
     End Class
 
     Class Duck
         Inherits Bird
+        Public Shadows Function Move() As String
+            Return "It walks like a duck..."
+        End Function
+        Public Shadows Function speak() As String
+            Return "It quacks like a duck..."
+        End Function
     End Class
 
 End Module
